eb61e19cb81c1854e18080b92b9108e5fd2b0ecb
MITHUN KHANJIR <Mithun@MITHUN>
2018-10-14 02:57:39 -0400

    API Enemy Behaviors and a new Scene
    Created a new scene that will be the base of the game. Added Enemy Follow and Enemy Patrol Scripts for the AI.

---
+++ Capstone-Dev/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Follow the player everywhere
+public class EnemyFollow : MonoBehaviour
+{
+
+    public float speed;
+
+    private Transform target;
+
+    void Start()
+    {
+        //Set player as the target
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, target.position) > 1)
+        {
+            //move enemy to the player's position
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        
+    }
+
+}
