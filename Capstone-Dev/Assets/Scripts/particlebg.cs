a66609c7e23c4c781bf24341d1e9b78569713873
Chuan Ren (RIT Student) <cr3186@ad.rit.edu>
2018-11-08 11:37:09 -0500

    particle1

---
+++ Capstone-Dev/Assets/Scripts/particlebg.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particlebg : MonoBehaviour {
+
+    public ParticleSystem ParticleCons;
+    public ParticleSystem ParticleDes;
+
+ 
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            ParticleCons.Stop();
+
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ParticleCons.Play();
+        }
+
+    }
+
+
+    public void OnParticleSystemStopped()
+    {
+       
+
+    }
+}
---
+++ Capstone-Dev/Assets/Scripts/particlecent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particlecent : MonoBehaviour {
+
+
+    public ParticleSystem ParticleCent;
+    public ParticleSystem ParticleDes;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            ParticleCent.Clear();
+            ParticleCent.Stop();
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ParticleCent.Play();
+        }
+
+    }
+
+
+    public void OnParticleSystemStopped()
+    {
+        ParticleDes.Play();
+
+    }
+}
