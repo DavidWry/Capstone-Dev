0188904be0ce833828c2592eeb44d53af83863a2
DavidWry <rxw7522@rit.edu>
2018-11-29 03:40:48 -0500

    Simple Power Bar

---
+++ Capstone-Dev/Assets/Scripts/UITemp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using AssemblyCSharp;
+
+public class UITemp : MonoBehaviour {
+
+    public GameObject PlayerObj;
+    public float AP = 0;
+    public Slider PowerBar;
+    public float percentage;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        AP = PlayerObj.GetComponent<Player>().Power;
+        percentage = AP / 100f;
+        PowerBar.value = percentage;
+	}
+}
