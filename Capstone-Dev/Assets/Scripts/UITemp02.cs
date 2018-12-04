cb3f857811f3cd753e9f1d1d63be8f6e0d532c60
DavidWry <rxw7522@rit.edu>
2018-12-04 04:57:15 -0500

    UI hp fixed

---
+++ Capstone-Dev/Assets/Scripts/UITemp02.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using AssemblyCSharp;
+
+public class UITemp02 : MonoBehaviour {
+
+    public GameObject PlayerObj;
+    Image image;
+    float Hp = 100;
+
+	// Use this for initialization
+	void Start () {
+        image = gameObject.GetComponent<Image>();
+        PlayerObj = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!PlayerObj)
+            PlayerObj = GameObject.FindGameObjectWithTag("Player");
+        else
+            Hp = PlayerObj.GetComponent<Player>().HitPoint;
+        image.fillAmount = Hp / 100;
+	}
+}
