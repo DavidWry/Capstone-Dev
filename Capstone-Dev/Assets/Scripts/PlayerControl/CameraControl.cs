3c2b3e1a2341874eaa43cdff9954594f14cf53be
junliangli <junliangli@users.noreply.github.com>
2018-12-03 21:41:37 -0500

    change some prefab

--- Capstone-Dev/Assets/Scripts/PlayerControl/CameraControl.cs
+++ Capstone-Dev/Assets/Scripts/PlayerControl/CameraControl.cs
@@ -22,29 +22,29 @@ public class CameraControl : MonoBehaviour {
         if(!PlayerPos)
             PlayerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         Vector3 pos = transform.position;
-        if (PlayerPos.position.x > border.x + 15f && PlayerPos.position.x < border2.x - 15f)
+        if (PlayerPos.position.x > border.x + 17 && PlayerPos.position.x < border2.x - 17)
         {
             pos.x = PlayerPos.position.x;
         }
-        else if (PlayerPos.position.x < border.x + 15f)
+        else if (PlayerPos.position.x < border.x + 17)
         {
-            pos.x = border.x + 15f;
+            pos.x = border.x + 17;
         }
-        else if (PlayerPos.position.x > border2.x - 15f)
+        else if (PlayerPos.position.x > border2.x - 17)
         {
-            pos.x = border2.x - 15f;
+            pos.x = border2.x - 17;
         }
-        if (PlayerPos.position.y > border.y+5 && PlayerPos.position.y < border2.y-5)
+        if (PlayerPos.position.y > border.y+9.56f && PlayerPos.position.y < border2.y-9.56f)
         {
             pos.y = PlayerPos.position.y;
         }
-        else if (PlayerPos.position.y < border.y + 5)
+        else if (PlayerPos.position.y < border.y + 9.56f)
         {
-            pos.y = border.y + 5;
+            pos.y = border.y + 9.56f;
         }
-        else if (PlayerPos.position.y > border2.y - 5)
+        else if (PlayerPos.position.y > border2.y - 9.56f)
         {
-            pos.y = border2.y - 5;
+            pos.y = border2.y - 9.56f;
         }
         transform.position = pos;
     }
