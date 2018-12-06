29d82fe993d6fa65dceebf385b2ac43af13913c1
Chuan Ren (RIT Student) <cr3186@ad.rit.edu>
2018-12-06 13:08:25 -0500

    bossupdate1

--- Capstone-Dev/Assets/Scripts/Fsmandhp.cs
+++ Capstone-Dev/Assets/Scripts/Fsmandhp.cs
@@ -4,42 +4,86 @@ using UnityEngine;
 
 public class Fsmandhp : MonoBehaviour {
 
-    public float hp = 25;
+    public float hp = 100;
     public GameObject jiguang;
     public GameObject toufazuo;
     public GameObject toufayou;
     private GameObject a;
-
-
+    private float DistanceBP;
+    private float DistanceBO;
+    private GameObject player;
+    private float maxpos=100;
+    private Vector3 originalpos;
+    private bool supposetomove;
+    private int yuancount=0;
    private Animator anim;
 	// Use this for initialization
 	void Start () {
         a = gameObject;
         anim = a.GetComponent<Animator>();
+        originalpos = gameObject.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-
-
-        if (hp < 50)
+        if (hp <= 30 && yuancount == 1)
         {
-            int randomNum = (int)Random.Range(1, 6);
-            anim.SetInteger("stage", randomNum);
+            yuancount++;
+            supposetomove = true;
+
+
+        }
+       else if (hp <= 50 && yuancount == 0)
+        {
+            yuancount++;
+            supposetomove = true;
+
 
 
         }
 
-        else
+
+
+
+
+
+
+
+        if (!supposetomove)
         {
-            int randomNum = (int)Random.Range(1, 5);
-            anim.SetInteger("stage", randomNum);
-            
+            DistanceBP = Vector3.Distance(transform.position, player.transform.position);
+            DistanceBO = Vector3.Distance(transform.position, originalpos);
+            if (DistanceBP < maxpos && DistanceBO < maxpos)
+            { 
+                if (hp < 50)
+                {
+                    int randomNum = (int)Random.Range(1, 6);
+                    anim.SetInteger("stage", randomNum);
+
+
+                }
+
+                else
+                {
+                    int randomNum = (int)Random.Range(1, 5);
+                    anim.SetInteger("stage", randomNum);
+
+
+                }
+            }
+
+
+            else
+            {
+                anim.SetInteger("stage", -1);
+
+            }
+
+
 
         }
-    
-
+        
 
 	}
 
