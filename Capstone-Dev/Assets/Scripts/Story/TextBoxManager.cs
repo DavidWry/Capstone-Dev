3e89e5a9b7b3747db36e32e3c6990bb741c9ccf2
DavidWry <rxw7522@rit.edu>
2019-01-19 23:52:07 -0500

    Added text boxes.

---
+++ Capstone-Dev/Assets/Scripts/Story/TextBoxManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour {
+    public GameObject TextBox;
+    public Text TextContent;
+
+    public TextAsset TextFile;
+    public string[] textLines;
+
+    private int currentLine;
+    private int endOfLine;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (TextFile != null)
+        {
+            textLines = (TextFile.text.Split('\n'));
+        }
+        if (endOfLine == 0)
+        {
+            endOfLine = textLines.Length - 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextContent.text = textLines[currentLine];
+
+        if (Input.GetButtonDown("AButton"))
+        {
+            currentLine ++;
+        }
+    }
+}
