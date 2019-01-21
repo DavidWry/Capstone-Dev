26af976cf8c8efc6952ec3a3a2cbf688d9a680df
DavidWry <rxw7522@rit.edu>
2019-01-20 20:31:09 -0500

    Talk with typing anime

---
+++ Capstone-Dev/Assets/Scripts/Story/NPC.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+    public TextBoxManager theTextBox;
+
+	// Use this for initialization
+	void Start () {
+        theTextBox = FindObjectOfType<TextBoxManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetButtonDown("AButton"))
+            {
+                theTextBox.EnableTextBox();
+            }
+        }
+    }
+}
--- Capstone-Dev/Assets/Scripts/Story/TextBoxManager.cs
+++ Capstone-Dev/Assets/Scripts/Story/TextBoxManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class TextBoxManager : MonoBehaviour {
     public GameObject TextBox;
+    public GameObject Player;
     public Text TextContent;
 
     public TextAsset TextFile;
@@ -13,9 +14,17 @@ public class TextBoxManager : MonoBehaviour {
     private int currentLine;
     private int endOfLine;
 
+    private bool isTyping = false;
+    private bool cancelTyping = false;
+
+    [SerializeField]
+    private float typingSpeed = 0.02f;
+
     // Use this for initialization
     void Start()
     {
+        currentLine = 0;
+        Player = GameObject.FindWithTag("Player");
         if (TextFile != null)
         {
             textLines = (TextFile.text.Split('\n'));
@@ -29,11 +38,60 @@ public class TextBoxManager : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        TextContent.text = textLines[currentLine];
+        //TextContent.text = textLines[currentLine];
 
         if (Input.GetButtonDown("AButton"))
         {
-            currentLine ++;
+            if (!isTyping)
+            {
+                currentLine ++;
+                if (currentLine > endOfLine)
+                {
+                    DisableTextBox();
+                }
+                else
+                {
+                    StartCoroutine(TextScroll(textLines[currentLine]));
+                }
+            }
+            else if (isTyping && !cancelTyping)
+            {
+                cancelTyping = true;
+            }
         }
+
+    }
+
+    private IEnumerator TextScroll (string lineOfText)
+    {
+        int letter = 0;
+        TextContent.text = "";
+        isTyping = true;
+        cancelTyping = false;
+        while(isTyping && !cancelTyping && (letter < lineOfText.Length - 1))
+        {
+            TextContent.text += lineOfText[letter];
+            letter++;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+        TextContent.text = lineOfText;
+        isTyping = false;
+        cancelTyping = false;
+    }
+
+    public void EnableTextBox()
+    {
+        TextBox.SetActive(true);
+        Player.GetComponent<Movement>().enabled = false;
+        Player.GetComponent<Shoot>().enabled = false;
+        //StartCoroutine(TextScroll(textLines[currentLine]));
+    }
+
+    public void DisableTextBox()
+    {
+        currentLine = 0;
+        TextBox.SetActive(false);
+        Player.GetComponent<Movement>().enabled = true;
+        Player.GetComponent<Shoot>().enabled = true;
     }
 }
