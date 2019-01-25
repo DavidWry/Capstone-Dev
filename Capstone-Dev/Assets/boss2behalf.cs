af06ed3ff4160db2eafc88986637f749ecdbdd6c
Chuan Ren (RIT Student) <cr3186@ad.rit.edu>
2019-01-25 02:44:35 -0500

    boss2update3

---
+++ Capstone-Dev/Assets/boss2behalf.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boss2behalf : MonoBehaviour {
+    GameObject boss2Sprite;
+    Animator boss2Anim;
+
+    float idleTime = 0;
+    // Use this for initialization
+    void Start () {
+        boss2Sprite = gameObject.transform.GetChild(0).gameObject;
+        boss2Anim = boss2Sprite.GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        int p = (int)Random.Range(0, 6);
+
+        if (boss2Anim.GetCurrentAnimatorClipInfo(0)[0].clip.name == "row1") {
+            idleTime += Time.deltaTime;
+            if (idleTime >= 2)
+            {
+                boss2Anim.SetInteger("state", p);
+                idleTime = -1;
+            }
+            
+        }
+        else { idleTime = 0; boss2Anim.SetInteger("state", -1); }
+
+
+
+
+    }
+
+}
---
+++ Capstone-Dev/Assets/boss2charge.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boss2charge : MonoBehaviour {
+    int waitcount=0;
+    float chargetime = 0;
+    public GameObject chargeSprite;
+    Vector3 unitvec;
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (waitcount > 3)
+        {
+            waitcount = 0;
+            gameObject.GetComponent<Animator>().SetBool("canCharge", true);
+            unitvec = (GameObject.FindGameObjectWithTag("Player").gameObject.transform.position-gameObject.transform.parent.gameObject.transform.position).normalized;
+        }
+
+
+
+        if (gameObject.GetComponent<Animator>().GetCurrentAnimatorClipInfo(0)[0].clip.name == "row2" )
+        {
+
+            if (chargetime < 3 || Vector3.Distance(GameObject.FindGameObjectWithTag("Player").gameObject.transform.position, gameObject.transform.parent.gameObject.transform.position) < 5)
+            {
+                gameObject.GetComponentInParent<Rigidbody>().MovePosition(gameObject.transform.parent.gameObject.transform.position + unitvec*Time.deltaTime*50);
+                chargetime += Time.deltaTime;
+                chargeSprite.SetActive(true);
+                gameObject.GetComponent<Animator>().SetBool("canCharge", false);
+            }
+            else
+            {
+                chargeSprite.SetActive(false);
+                gameObject.GetComponent<Animator>().SetBool("reach", true);
+             
+                chargetime = 0;
+            }
+        }
+    
+
+        if (gameObject.GetComponent<Animator>().GetCurrentAnimatorClipInfo(0)[0].clip.name == "row1")
+        {
+            chargeSprite.SetActive(false);
+            gameObject.GetComponent<Animator>().SetBool("reach", false);
+
+        }
+
+
+
+    }
+
+    public void addwaitcount()
+    {
+        waitcount++;
+
+    }
+
+    public void facedirection()
+    {
+        float p=(GameObject.FindGameObjectWithTag("Player").gameObject.transform.position - gameObject.transform.parent.gameObject.transform.position).x;
+        if (p < 0) {
+            gameObject.transform.parent.transform.rotation = Quaternion.Euler(0, 180, 0);
+                
+                }
+        else
+        {
+            gameObject.transform.parent.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.GetComponent<Animator>().GetCurrentAnimatorClipInfo(0)[0].clip.name == "row2"&&other.gameObject.tag!="Player")
+        {
+            unitvec *= -1;
+            if (gameObject.transform.parent.transform.rotation.y == 180)
+            {
+                gameObject.transform.parent.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+            }
+            else { gameObject.transform.parent.transform.rotation = Quaternion.Euler(0, 180, 0); }
+
+
+        };
+
+
+    }
+
+}
