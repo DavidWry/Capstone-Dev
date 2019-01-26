9dff55a5beea1f343b2b6cf2635bc185e72ffff0
Chuan Ren (RIT Student) <cr3186@ad.rit.edu>
2019-01-26 00:13:24 -0500

    boss2update4

---
+++ Capstone-Dev/Assets/boss2slam.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boss2slam : MonoBehaviour {
+    public GameObject slamParticle;
+    public ParticleSystem slamParticleSystem;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!slamParticleSystem.isPlaying)
+        {
+
+            slamParticle.SetActive(false);
+
+        }
+
+	}
+
+
+    public void slam()
+    {
+        slamParticle.SetActive(true);
+        slamParticleSystem.Play();
+
+    }
+}
---
+++ Capstone-Dev/Assets/boss2stab.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boss2stab : MonoBehaviour {
+    public GameObject stabSprite;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void stabStart()
+    {
+        stabSprite.SetActive(true);
+    }
+    public void stabEnd()
+    {
+        stabSprite.SetActive(false);
+    }
+}
