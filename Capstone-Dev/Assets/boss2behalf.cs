06fc5f567112f7d9325fcf98b327bf2a2cde9b88
User <User@DESKTOP-IDFQMUR>
2019-01-28 17:23:08 -0500

    bossupdate5

--- Capstone-Dev/Assets/boss2behalf.cs
+++ Capstone-Dev/Assets/boss2behalf.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class boss2behalf : MonoBehaviour {
     GameObject boss2Sprite;
     Animator boss2Anim;
-
+    int breathState = 0;
     float idleTime = 0;
+    int hp = 500;
     // Use this for initialization
     void Start () {
         boss2Sprite = gameObject.transform.GetChild(0).gameObject;
@@ -17,20 +18,37 @@ public class boss2behalf : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         int p = (int)Random.Range(0, 6);
-
-        if (boss2Anim.GetCurrentAnimatorClipInfo(0)[0].clip.name == "row1") {
-            idleTime += Time.deltaTime;
-            if (idleTime >= 2)
-            {
-                boss2Anim.SetInteger("state", p);
-                idleTime = -1;
-            }
-            
+        if (hp <= 375 && breathState == 0&& boss2Anim.GetCurrentAnimatorClipInfo(0)[0].clip.name == "row1")
+        {
+            boss2Anim.SetInteger("state", 9);
+            breathState++;
         }
-        else { idleTime = 0; boss2Anim.SetInteger("state", -1); }
+        else if (breathState == 1 && hp <= 250&& boss2Anim.GetCurrentAnimatorClipInfo(0)[0].clip.name == "row1")
+        {
+            boss2Anim.SetInteger("state", 9);
+            breathState++;
+        }
+        else if (hp <= 0)
+        {
+            boss2Anim.SetInteger("state", 10);
+        }
+        else
+        {
+            if (boss2Anim.GetCurrentAnimatorClipInfo(0)[0].clip.name == "row1")
+            {
+
+                idleTime += Time.deltaTime;
+                if (idleTime >= 2)
+                {
+                    boss2Anim.SetInteger("state", p);
+                    idleTime = -1;
+                }
+
+            }
+            else { idleTime = 0; boss2Anim.SetInteger("state", -1); }
 
 
-
+        }
 
     }
 
---
+++ Capstone-Dev/Assets/boss2whirlwind.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boss2whirlwind : MonoBehaviour {
+
+    public GameObject windParticle;
+    public ParticleSystem windParticleSystem;
+    public Animator bossAnimator;
+    float timeCount=0;
+    Vector3 nextSpot;
+    // Use this for initialization
+    void Start()
+    {
+        bossAnimator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (windParticleSystem.isPlaying)
+        {
+            if (timeCount == 0)
+            {
+                nextSpot = new Vector3(0, 0, 0);
+               
+               timeCount = 1;
+            }
+           
+            else
+            {
+                Vector3 addition = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0) * Time.deltaTime;
+                print(addition);
+                nextSpot += addition;
+               
+                gameObject.transform.parent.transform.position += nextSpot;
+
+            }
+
+
+        }
+        else
+        {
+            nextSpot = new Vector3(0, 0, 0);
+
+            timeCount = 0;
+        }
+    }
+
+    public void whrilStart()
+    {
+        windParticleSystem.Play();
+
+    }
+
+    public void whrilEnd()
+    {
+        windParticleSystem.Stop();
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (windParticleSystem.isPlaying&&other.tag != "Player")
+        {
+            nextSpot *= -1;
+        }
+
+    }
+
+}
