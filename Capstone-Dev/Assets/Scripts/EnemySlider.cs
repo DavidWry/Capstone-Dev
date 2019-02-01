f23863e91904ca32ab8536fb3452a19c6c6fa6b0
MITHUN KHANJIR <Mithun@MITHUN>
2019-02-01 00:12:25 -0500

    Sliding Enemy behavior fixed and modifications

--- Capstone-Dev/Assets/Scripts/EnemySlider.cs
+++ Capstone-Dev/Assets/Scripts/EnemySlider.cs
@@ -13,11 +13,18 @@ public class EnemySlider : MonoBehaviour
 
     private Player_New player;
     private Transform target;
+    private Vector3 targetPos;
 
+
+    private int health;
     private float rangeForAttack;
+    private int damage;
     private Vector3 dir;
 
     private Animator anim;
+    private bool hasCollided ;
+    private bool hasReached;
+
 
     void Start ()
     {
@@ -26,43 +33,115 @@ public class EnemySlider : MonoBehaviour
         canDash = true;
         dashTime = 2f;
         rangeForAttack = 6f;
+        damage = 8;
 
         player = GetComponent<Player_New>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
         anim = GetComponent<Animator>();
-
-
+        hasCollided = false;
+        hasReached = false;
     }
-	
-	// Update is called once per frame
-	void FixedUpdate ()
+
+    // Update is called once per frame
+    void Update()
     {
-       
+       //if player isnt dead
         if (target != null)
         {
-            if((Vector3.Distance(transform.position, target.position) <= rangeForAttack) && (canDash == true))
+            //Make the enemy face the player
+            var scale = transform.localScale;
+            scale.x = Mathf.Abs(scale.x);
+            if (target.position.x < transform.position.x)
             {
+                scale.x *= -1;
+            }
+            transform.localScale = scale;
+            
+            //check if under range and if he can dash
+            if( (Vector3.Distance(transform.position, target.position) <= rangeForAttack) && (canDash == true))
+            {
+                targetPos = new Vector3(target.position.x, target.position.y,target.position.z);
                 anim.SetTrigger("Sliding");
-                dir = (target.position - transform.position).normalized * dashSpeed;
+                dir = (targetPos - transform.position).normalized * dashSpeed;
                 rb.velocity = dir;
                 canDash = false;
+                hasReached = false;
             }
-            if (Vector3.Distance(transform.position, target.position) <= 0.1f)
+
+            //has the enemy reached the target position
+            if (Vector2.Distance(transform.position, targetPos) <= 0.1f)
             {
-                anim.SetBool("isRunning", false);
-                dashTime -= Time.deltaTime;
-                canDash = false;
-                rb.velocity = new Vector3(0f, 0f, 0f);
-               // transform.position = target.position;
+                hasReached = true;
                 
+                   
+
             }
+            if (hasReached == true)
+            {
+                canDash = false;
+                anim.SetBool("isRunning", false);
+                rb.velocity = Vector3.zero;
+                dashTime -= Time.deltaTime;
+            }
+
+            //if the countdown for the next dash becomes zero
             if (dashTime <= 0)
             {
-                canDash = true;
+               canDash = true;
                 dashTime = 2f;
                 target = GameObject.FindGameObjectWithTag("Player").transform; 
             }
 
         }
 	}
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        
+        
+        if (other.gameObject.tag == "Player")
+         {
+            if (hasCollided == false)
+            {
+                hasCollided = true;
+                rb.velocity = Vector3.zero;
+                anim.SetBool("isRunning", false);
+                other.gameObject.GetComponent<Player_New>().TakeDamage(damage);
+                // StartCoroutine(IfCollidedWithPlayer(2f));
+                hasReached = true;
+                hasCollided = false;
+               // canDash = true;
+
+               // dashTime = 2f;
+            //    target = GameObject.FindGameObjectWithTag("Player").transform;
+            }
+         
+
+        }
+    
+       
+
+        if(other.gameObject.tag == "Obstacle")
+        {
+            anim.SetBool("isRunning", false);
+            rb.velocity = Vector3.zero;
+            hasReached = true;
+           // StartCoroutine(IfCollidedWithPlayer(2f));
+          // canDash = true;
+        
+
+        }
+       
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+
+    IEnumerator IfCollidedWithPlayer(float newWaitTime)
+    {
+        yield return new WaitForSeconds(newWaitTime);
+    }
+
+
 }
