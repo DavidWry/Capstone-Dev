ffb8496e7a6e0f88d23cb909efab1ea45b28acd8
DavidWry <rxw7522@rit.edu>
2019-02-10 19:31:41 -0500

    Main Room update

---
+++ Capstone-Dev/Assets/Scripts/MainRoom/Anime_Door.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anime_Door : MonoBehaviour {
+
+    Animator anime;
+
+	// Use this for initialization
+	void Start () {
+        anime = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anime.SetBool("Open", true);
+        anime.SetBool("Close", false);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        anime.SetBool("Close", true);
+        anime.SetBool("Open", false);
+    }
+}
