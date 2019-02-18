d72c9a7fbbcf5a2d2ef6da42868ea7f9de68ab57
junliangli <junliangli@users.noreply.github.com>
2019-02-18 15:01:49 -0500

    fix chest position bug

--- Capstone-Dev/Assets/Scripts/ProceduralGeneration/Chest.cs
+++ Capstone-Dev/Assets/Scripts/ProceduralGeneration/Chest.cs
@@ -20,8 +20,8 @@ public class Chest : MonoBehaviour {
         {
             string tempName = probability.DetermineDrop();
             GameObject itemObj = gameManager.GetItemObj(tempName);
-            itemObj = Instantiate(gameManager.GetItemObj(tempName), transform.position+new Vector3(1.28f,1.28f,0), Quaternion.Euler(0, 0, 0));
-            itemObj.transform.localScale = new Vector3(4, 4, 4);
+            itemObj = Instantiate(gameManager.GetItemObj(tempName), transform.position, Quaternion.Euler(0, 0, 0));
+            itemObj.transform.localScale = new Vector3(30, 30, 30);
             var worldCanvas = GameObject.Find("worldCanvas").transform;
             itemObj.transform.parent = worldCanvas;
             Destroy(gameObject);
