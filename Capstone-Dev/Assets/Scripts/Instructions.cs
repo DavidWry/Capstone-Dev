592454565571432fa91106873e50652dcfb7272b
MITHUN KHANJIR <Mithun@MITHUN>
2019-02-18 04:14:28 -0500

    New Tutorial
    Now the tutorial has a new style with the latest UI and font.
    
    The instructions fade in when youu enter the area and stays like that until you are inside the trigger
    
    The instructions fade out when you exit the area

---
+++ Capstone-Dev/Assets/Scripts/Instructions.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instructions : MonoBehaviour {
+
+
+    private SpriteRenderer sprite;
+    private bool activate;
+    private bool hasLeft;
+    private float showTime;
+
+    private Color color;
+	void Start ()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        activate = false;
+        hasLeft = false;
+        showTime = 3.0f;
+        color = sprite.color;
+        color.a = 0;
+        sprite.color = color;  
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //has player entered the trigger
+        if (activate == true)
+        {
+            color = sprite.color;
+            color.a += (Time.deltaTime / 2);
+            
+            // showTime -= Time.deltaTime;
+            sprite.color = color;
+
+        }
+
+        //player has entered and text has been fully shown
+        if(color.a > 1)
+        {
+           color.a = 1;
+            activate = false;
+        }
+       
+        // destroy the trigger after player has exited and some time has passed.
+        if (showTime <= 0)
+        {
+              Destroy(gameObject);
+        }
+
+        //if player has exited the trigger, fade the text 
+        if (hasLeft == true)
+        {
+            color = sprite.color;
+            color.a -= (Time.deltaTime / 2);
+            showTime -= Time.deltaTime;
+            sprite.color = color;
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            activate = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            hasLeft = true;
+        }
+    }
+}
