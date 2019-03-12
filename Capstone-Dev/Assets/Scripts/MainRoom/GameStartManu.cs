a55a77c4f0940fe2b735a3f0bbb9cc5609696e3d
DavidWry <rxw7522@rit.edu>
2019-03-12 01:44:53 -0400

    Main Room ui fix again.

--- Capstone-Dev/Assets/Scripts/MainRoom/GameStartManu.cs
+++ Capstone-Dev/Assets/Scripts/MainRoom/GameStartManu.cs
@@ -9,6 +9,7 @@ public class GameStartManu : MonoBehaviour {
     public GameObject Menu;
     GameObject Player;
     InterfaceAnimManager manager;
+    bool isOff = true;
 
 	// Use this for initialization
 	void Start () {
@@ -30,15 +31,17 @@ public class GameStartManu : MonoBehaviour {
         {
             if (Menu)
             {
-                if (Input.GetButtonDown("AButton"))
+                if (Input.GetButtonDown("AButton") && isOff)
                 {
                     Player.GetComponent<Movement_New>().enabled = false;
                     Menu.SetActive(true);
+                    isOff = false;
                 }
                 else if (Input.GetButtonDown("BButton"))
                 {
                     Player.GetComponent<Movement_New>().enabled = true;
                     Menu.SetActive(false);
+                    isOff = true;
                 }
             }
         }
