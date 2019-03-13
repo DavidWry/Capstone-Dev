fc06454e89cf78b4a251042c7dda745425e35e89
Chuan Ren (RIT Student) <cr3186@ad.rit.edu>
2019-03-13 00:23:25 -0400

    dayum

---
+++ Capstone-Dev/Assets/MinibossHealthbar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MinibossHealthbar : MonoBehaviour {
+
+    // Use this for initialization
+    Image healthBar;
+    public GameObject fsm;
+    // Use this for initialization
+    void Start()
+    {
+        healthBar = gameObject.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.fillAmount = (fsm.GetComponent<MiniBoss>().HP / 100.0f);
+        if (healthBar.fillAmount <= 0)
+        {
+            Destroy(healthBar);
+        }
+    }
+}
