94b91fb4db9801f34d01f9c1397d4aeb76178afa
MITHUN KHANJIR <Mithun@MITHUN>
2019-04-11 10:08:32 -0400

    WindEffect Prototype

---
+++ Capstone-Dev/Assets/WindEffect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindEffect : MonoBehaviour {
+
+    public float Strength;
+    public Vector3 WindDirection;
+    public float speed;
+    public GameObject effect;
+
+    private void Update()
+    {
+        transform.position += new Vector3(speed,0,0);
+       // Instantiate(effect, transform.position, Quaternion.identity);
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        Rigidbody colRigidbody = col.GetComponent<Rigidbody>();
+        if (colRigidbody != null)
+        {
+            colRigidbody.AddForce(WindDirection * Strength);
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        Rigidbody colRigidbody = col.GetComponent<Rigidbody>();
+        if (colRigidbody != null)
+        {
+            colRigidbody.AddForce(WindDirection * Strength);
+        }
+    }
+}
