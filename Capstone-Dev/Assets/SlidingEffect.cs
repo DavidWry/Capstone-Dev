810db62fe5b72e2410063d7606cd198a5fc380a3
MITHUN KHANJIR <Mithun@MITHUN>
2019-04-19 02:14:17 -0400

    Work on Ice tiles
    The material based movement is not working for some reason, will come up with some new technique to solve this

---
+++ Capstone-Dev/Assets/SlidingEffect.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingEffect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GetComponent<BoxCollider>().material.dynamicFriction = 0;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GetComponent<BoxCollider>().material.dynamicFriction = 0;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GetComponent<BoxCollider>().material.dynamicFriction = 1;
+        }
+    }
+
+
+}
